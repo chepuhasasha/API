e53ec7cd02235b6eece56167cda61fa1bc46ccc7
Sergey Batukov <sashachepuha@gmail.com>
2022-01-23 20:46:20 +0300

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { BaseController } from "../common/base.controller";
+import { HTTPError } from "../errors/http-error.class";
 import { LogerService } from "../logger/logger.service";
 
 export class UserController extends BaseController {
@@ -12,7 +13,7 @@ export class UserController extends BaseController {
   }
 
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, "login");
+    next(new HTTPError(401, "auth error", "login"));
   }
 
   register(req: Request, res: Response, next: NextFunction) {
