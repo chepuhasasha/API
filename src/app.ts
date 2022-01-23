45bd09d4a3f1be951eddecb8d0cc19d160559ca6
Sergey Batukov <sashachepuha@gmail.com>
2022-01-23 20:51:09 +0300

    add errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { ExeptionFilter } from "./errors/exeption.filter";
 import { ILogger } from "./logger/logger.interface";
 import { UserController } from "./users/users.controller";
 
@@ -9,19 +10,30 @@ export class App {
   server: Server;
   logger: ILogger;
   userController: UserController;
-  constructor(logger: ILogger, userController: UserController) {
+  exeptionFilter: ExeptionFilter;
+  constructor(
+    logger: ILogger,
+    userController: UserController,
+    exeptionFilter: ExeptionFilter
+  ) {
     this.app = express();
     this.port = 3000;
     this.logger = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router);
   }
 
+  useExeptionFiltres() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+  }
+
   public async init() {
     this.useRoutes();
+    this.useExeptionFiltres();
     this.server = this.app.listen(this.port);
     this.logger.log(`server started on PORT: ${this.port}`);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { App } from "./app";
+import { ExeptionFilter } from "./errors/exeption.filter";
 import { LogerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
 
 async function bootstrap() {
   const logger = new LogerService();
-  const app = new App(logger, new UserController(logger));
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExeptionFilter(logger)
+  );
   await app.init();
 }
 
